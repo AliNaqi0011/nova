aab1eb67f333fef5e4cf735d56716e1fe4b6927f
Ali Naqi <alinaqi.te@gmail.com>
2025-08-16 18:18:35 +0000

    nechhy sa powered by and e resume wala rmv krdo

--- src/templates/simple/components/Footer.tsx
+++ src/templates/simple/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer({ url }: { url: string }) {
   return (
     <footer className="flex justify-between items-center text-xs text-gray-400 mt-6 pt-2 border-t">
       <span>{url ? url.replace('https://', '') : 'www.yourwebsite.com'}</span>
-      <span>Powered by Nova Resume</span>
+      <span />
     </footer>
   );
 }
